2004c0b77eb350a8d35f3afa1a89fac545b33a30
sureshsubedii <sureshsubedi485@gmail.com>
2024-09-10 19:00:29 +0545

    Optimize insertion sort

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,13 +1,29 @@
+// const insertionSort = (nums) =>{
+//     for(let i = 1; i < nums.length; i ++){
+//         let counter = i
+//         while(counter && nums[counter - 1] > nums[counter]){
+//             let temp = nums[counter - 1]
+//             nums[counter - 1] = nums[counter]
+//             nums[counter] = temp
+//             counter --
+//         }
+//     }
+//     return nums
+// }
+
 const insertionSort = (nums) =>{
     for(let i = 1; i < nums.length; i ++){
-        let counter = i
-        while(counter && nums[counter - 1] > nums[counter]){
-            let temp = nums[counter - 1]
-            nums[counter - 1] = nums[counter]
-            nums[counter] = temp
-            counter --
+        let key = nums[i]
+        let j = i - 1
+        while( j >=0 && nums[j] > key){
+            nums[j + 1] = nums[j]
+            j -- 
+            nums[ j + 1] =key
         }
     }
     return nums
+
 }
-console.log(insertionSort([3, 1, 4, 1, 5,0]))
+
+
+console.log(insertionSort([3, 1, 4, 1, 5,-1]))
